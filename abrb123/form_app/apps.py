d71a12689aa4d7e73599ea95714e5a77fe9c07b1
krupanu <Krupanu2@gmail.com>
2025-08-20 16:55:56 +0300

    Final adjustments

--- abrb123/form_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class FormappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'form_app'
--- abrb123/form_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import DynamicFormView, FormSubmitView, DataListView
-
-urlpatterns = [
-    path('', DynamicFormView.as_view(), name='form'),
-    path('submit/', FormSubmitView.as_view(), name='submit'),
-    path('list/', DataListView.as_view(), name='list'),
-]
